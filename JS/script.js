7b8407dc09c57d6de29803b647b60a61acd344c4
Hady Hany <hadyharb1988@gmail.com>
2024-08-25 13:09:39 +0300

    category section

--- JS/script.js
+++ JS/script.js
@@ -10,6 +10,20 @@ document.querySelector("#nav-close").onclick = () => {
 
 window.onscroll = () => {
   navbar.classList.remove("active");
+
+  if (window.scrollY > 0) {
+    document.querySelector(".header").classList.add("active");
+  } else {
+    document.querySelector(".header").classList.remove("active");
+  }
+};
+
+window.onload = () => {
+  if (window.scrollY > 0) {
+    document.querySelector(".header").classList.add("active");
+  } else {
+    document.querySelector(".header").classList.remove("active");
+  }
 };
 
 let searchForm = document.querySelector(".search-form");
